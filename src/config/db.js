ed9a36bf72882436d820ad7e25c53364d99f1593
FacundoOrt <46579266@comunidadort.edu.ar>
2026-06-08 20:38:32 -0300

    Configurar conexion MongoDB Atlas

--- src/config/db.js
+++ src/config/db.js
@@ -2,9 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            family: 4
-        });
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB conectado exitosamente');
     } catch (error) {
         console.error('Error al conectar a MongoDB:', error);
