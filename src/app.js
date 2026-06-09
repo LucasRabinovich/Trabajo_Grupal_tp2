891f585dc16d5bb14e6e330c4f78a2aa6838e896
lucas.mrabinovich@gmail.com <lucas.mrabinovich@gmail.com>
2026-06-09 08:44:20 -0300

    agrega documentacion de la API con Swagger

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 const authRoutes = require('./routes/auth.routes');
 const teamRoutes = require('./routes/team.routes');
 const playerRoutes = require('./routes/player.routes');
@@ -12,6 +13,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/teams', teamRoutes);
 app.use('/api/players', playerRoutes);
 app.use('/api/matches', matchRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/', (req, res) => {
     res.status(200).json({ message: "API DataGol corriendo correctamente" });
